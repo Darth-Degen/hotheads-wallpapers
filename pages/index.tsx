1aa710d2dc6a522b7d7364b3b2590b3cbae2082c
Darth Degen <darthdegen@gmail.com>
2023-04-04 13:15:47 -0700

    updated landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,29 +20,29 @@ const Home: NextPage = () => {
         <div className="px-12 md:px-8 flex-grow flex items-center ">
           <Image
             src="/images/logo_base.png"
-            width={3992 / 10}
-            height={1560 / 10}
+            width={3992 / 11}
+            height={1560 / 11}
             alt="Logo"
           />
         </div>
         {/* md + screens */}
         {/* <div className="hidden md:block relative w-screen h-1/3 lg:h-1/2 2xl:h-[70%] px-0 mx-0 py-10"> */}
-        <div className="hidden md:block relative w-screen min-h-1/2">
+        {/* <div className="hidden md:block relative w-screen min-h-1/2">
           <Image
             src="/images/underworld.png"
             alt="Underworld"
             width={3000}
             height={1002}
           />
-        </div>
+        </div> */}
         {/* sm - screens */}
-        <div className="md:hidden relative w-full h-[60%]">
+        <div className="relative w-[85%] lg:w-[95%] h-[55%] mx-10">
           <Image
             src="/images/underworld.png"
             fill={true}
             alt="Underworld"
             objectFit="cover"
-            className=""
+            className="rounded-sm"
           />
         </div>
         <div className="px-12 md:px-8 flex-grow flex items-center hover:saturate-200">
