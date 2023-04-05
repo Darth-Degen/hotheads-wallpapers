8a5795896dde5d7605796ba03c632196d616e750
Darth Degen <darthdegen@gmail.com>
2023-04-04 20:07:30 -0700

    added EA urls

--- src/constants/collections.ts
+++ src/constants/collections.ts
@@ -174,160 +174,160 @@ export const collections: Collection[] = [
   {
     id: 34,
     src:"/images/hotheads/hh_34/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/4fg3bUiK1aqNYkJern6yfb1txvAEqetcJ7vieVCABU3x"
   },
   {
     id: 35,
     src:"/images/hotheads/hh_35/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/FKXMRvDNkExFXKeBxSTNvCD39zbPoE9j53mZQ1183e5X"
   },
   {
     id: 36,
     src:"/images/hotheads/hh_36/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/548F92XGQLjY9qVLEdKbU2wibNMYEYtFKQqe2HWNQsH1"
   },
   {
     id: 37,
     src:"/images/hotheads/hh_37/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/C2d9kfE8kKtdkoPfsJJ9KsdpFc9uab74P2b73jdbnLxP"
   },
   {
     id: 38,
     src:"/images/hotheads/hh_38/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/EHRn9XRw57qKk22Tmza8FY7QDxDniGgbbJmABFaLuLHL"
   },
   {
     id: 39,
     src:"/images/hotheads/hh_39/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/D5KqJ4zgFT1H35gi1eiNoGixRzrETB6BsBFVZPyakVsw"
   },
   /* 40's */
   {
     id: 40,
     src:"/images/hotheads/hh_40/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/9ZvUnaNEiV9BGsyLME4LeZWGcEhg2ZFT1hC6wqeT3v6F"
   },
   {
     id: 41,
     src:"/images/hotheads/hh_41/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/2sXYz3ZHmHaoX9SKBXyA6WMyiqFarZDQSbZ69ARhKzAA"
   },
   {
     id: 42,
     src:"/images/hotheads/hh_42/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/CTezdWAmVxitEj11nTv2cGJ4GHM4neJU5MzyoTMpQfoD"
   },
   {
     id: 43,
     src:"/images/hotheads/hh_43/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/4GfDmLNaDR6dRb3Wk5fN7iRSH5fa9zthkXsa3UcNhi5S"
   },
   {
     id: 44,
     src:"/images/hotheads/hh_44/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/Bq8QsgiSXa9qGKDXM3mBMyfTtgVEnaQ81nA83Fzd97nS"
   },
   {
     id: 45,
     src:"/images/hotheads/hh_45/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/EKFUQKxSQUW2DHiwJtFqwzcnZjymhyooR11ckZUCQUgt"
   },
   {
     id: 46,
     src:"/images/hotheads/hh_46/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/32svEPmJRuokfK9UL4KnegnYaCSa1Td2RkK6diTEJcDj"
   },
   {
     id: 47,
     src:"/images/hotheads/hh_47/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/9uTAbxYEE5MD4G12etppeX4ueAD8cADQaiMZGjvM4Y3L"
   },
   {
     id: 48,
     src:"/images/hotheads/hh_48/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/22ZXRKy2gZwhM5yBBGxvM94W6eZ413o5VkFhux59KJoe"
   },
   {
     id: 49,
     src:"/images/hotheads/hh_49/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/J64KbwWhESDf8hYfiUQ9S1CWLriWVaJBeshis7bsKCRo"
   },
   /* 50's */
   {
     id: 50,
     src:"/images/hotheads/hh_50/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/3vz2Bno2Ah16LwQABeShdokaYt6yzKvXpdq7niRhcYVt"
   },
   {
     id: 51,
     src:"/images/hotheads/hh_51/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/2haSAW7GyTLrUa5dvFF2rrW5kBX3wMqPMtmEo335KQHH"
   },
   {
     id: 52,
     src:"/images/hotheads/hh_52/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/4q3pmMmVQzWrC1gUcoMR1Gsi7B8gKrLyNbJcBZhjZoi9"
   },
   {
     id: 53,
     src:"/images/hotheads/hh_53/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/Cebovku4S9EQXYuzoKbtd8fDz4JGnRBrGSSxCiFvJbY6"
   },
   {
     id: 54,
     src:"/images/hotheads/hh_54/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/6HzmGFwVzxSS7txsNSue5t3hnHZtrmTEMXLhUebLnpPS"
   },
   {
     id: 55,
     src:"/images/hotheads/hh_55/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/85Jooppbit14Xjp3bso9wjBCJLDnrxaKH47FvDFaTEvj"
   },
   {
     id: 56,
     src:"/images/hotheads/hh_56/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/FhnQAPSYiGi5mXjFzz3F188Tk549B9JAp9wu6kD7AbLc"
   },
   {
     id: 57,
     src:"/images/hotheads/hh_57/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/GBs4r8nSZF1yNzu27RhsiHJXodS35Y6JtJ5PXq2S4E9e"
   },
   {
     id: 58,
     src:"/images/hotheads/hh_58/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/4ad9GLUwreoY916jthrfRZJuCk4EzHAiGtTJ9XgRNnWD"
   },
   {
     id: 59,
     src:"/images/hotheads/hh_59/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/7JBZKnav366UpU1kbRt3SYDEXAaDrCTpGmP3ZUiGmprh"
   },
   /* 60's */
   {
     id: 60,
     src:"/images/hotheads/hh_60/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/Bj5s89j4TrvXRGsRhsA9WwsKZPnbVrDDdHpiB23Y6n2L"
   },
   {
     id: 61,
     src:"/images/hotheads/hh_61/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/zev9KJtgQdh6dXEy5P5rvbRQT6KYVDDvM68V6pDWd5C"
   },
   {
     id: 62,
     src:"/images/hotheads/hh_62/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/4f9ppSLLK9n4kB1wXdsCD39HtnPvAW1Wnjt4unCtsLJZ"
   },
   {
     id: 63,
     src:"/images/hotheads/hh_63/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/2coNgJjAqB1G8P6VbUuS6Hd5dy2p11qx1ZyePG5WhLDB"
   },
   {
     id: 64,
     src:"/images/hotheads/hh_64/PFP/image.png",
-    url:""
+    url:"https://exchange.art/single/EDhGrD6uBcfmWbHu5mfQGPQPhKPdUWQnW5vG6nUy7Hyh"
   },
   // {
   //   id: 65,
